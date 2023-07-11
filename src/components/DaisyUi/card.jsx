09258cf39ea260ee37345e66e8be4639e6c5677c
diako83 <ismaildiako@gmail.com>
2023-07-11 09:29:30 +0200

    ändra backkfrunden och tog bort stora tranglarna

--- src/components/DaisyUi/card.jsx
+++ src/components/DaisyUi/card.jsx
@@ -2,11 +2,11 @@ import imageSrc from '../../assets/img/cardbgone.jpg';
 
 const Card =(props)=>{
 return(
-  <div  className="card card-side bg-gradient-to-br  from-mainThemeColor from-80% to-lastNameColor to-10% shadow-xl m-5 expand flex-col lg:flex-row">
+  <div  className="card card-side bg-gradient-to-br  from-mainThemeColor from-80% to-lastNameColor to-10% shadow-xl m-5 expand flex-col lg:flex-row ">
     <figure><img src={props.imageSrc} alt="Movie" className='w-52 p-2'/></figure>
  
-  <div className="card-body w-96">
-    <h1 className="card-title ">{props.comp}</h1>
+  <div className="card-body w-96 text-xs md:text-lg">
+    <h1 className="card-title  ">{props.comp}</h1>
     <p className='text-2xl'>{props.title}</p>
     <p className='text-sm'>{props.date}</p>
     
